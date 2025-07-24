15a482529c6a4de84167553f5a1b47d8dfb8be21
hfansa <hfansa@hotmail.com>
2025-07-25 00:29:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    base: "/bllectual"
+    base: "/bllectual",
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
